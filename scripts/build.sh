c15ae999796be8bf964be79059a81e462849087c
Yannick Pereira-Reis <yannick@zol.fr>
2015-08-31 14:33:16 +0200

    Script fix

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,4 +2,4 @@ SATIS_PATH="/satis"
 SATIS_BIN="${SATIS_PATH}/bin/satis"
 SATIS_PUBLIC="${SATIS_PATH}/web/"
 
-${SATIS_BIN} -n build config.json ${SATIS_PUBLIC}
+${SATIS_BIN} -n build /app/config.json ${SATIS_PUBLIC}
