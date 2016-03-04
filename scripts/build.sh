4730bbdacaff709bf4f51c1ba99c512ee4e0ddb7
Stefan Bruggmann <stefan.bruggmann@dotpulse.ch>
2016-03-04 14:21:38 +0100

    bypass nginx statig file cache problems

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,3 +3,7 @@ SATIS_BIN="/satisfy/vendor/bin/satis"
 SATIS_PUBLIC="/satisfy/web/"
 
 ${SATIS_BIN} -n build /app/config.json ${SATIS_PUBLIC}
+
+# bypass nginx statig file cache problems..
+cat "${SATIS_PUBLIC}packages.json" > "${SATIS_PUBLIC}packages.json.tmp";
+mv "${SATIS_PUBLIC}packages.json.tmp" "${SATIS_PUBLIC}packages.json";
