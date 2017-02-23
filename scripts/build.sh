1792df9bafe076111d42a2a542e31903fc8625d9
Jérémy DECOOL <contact@jdecool.fr>
2017-02-23 09:09:44 +0100

    Fix satis bin path

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,5 @@
 SATIS_PATH="/satisfy/vendor"
-SATIS_BIN="/satisfy/vendor/bin/satis"
+SATIS_BIN="/satisfy/bin/satis"
 SATIS_PUBLIC="/satisfy/web/"
 
 ${SATIS_BIN} -n build /app/config.json ${SATIS_PUBLIC}
