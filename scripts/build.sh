e3e03823a7447acbc1abd89b560d155972594a9e
Tobias Matthaiou <tm@loberon.de>
2018-03-21 09:49:48 +0100

    Can be config over Docker environment

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-SATIS_PATH="/satisfy/vendor"
-SATIS_BIN="/satisfy/bin/satis"
-SATIS_PUBLIC="/satisfy/web/"
+SATIS_PATH=${SATIS_PATH:-"/satisfy/vendor"}
+SATIS_BIN=${SATIS_BIN:-"/satisfy/bin/satis"}
+SATIS_PUBLIC=${SATIS_PUBLIC:-"/satisfy/web/"}
 
 ${SATIS_BIN} -v -n build /app/config.json ${SATIS_PUBLIC}
